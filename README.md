fb49958a8d01f134dfed073da54c57e49dc95a1e
aliehs111 <128503077+aliehs111@users.noreply.github.com>
2023-08-05 15:01:19 -0400

    update readme and add notes to code

--- README.md
+++ README.md
@@ -15,18 +15,18 @@
  ## Installation
  Be sure to be running node.js.  Open live server at the server.js file and work with the user interface
  ## Usage
- xxx
+ To be used as a way to organize your notes and keep them for future reference.
  ## Tests
- xxx
+ No tests were created for this app
  ## License
  This project is licensed under the [MIT](https://opensource.org/licenses/MIT) license.
  ## Github Repository
- xx
+ https://github.com/aliehs111/Express.js-Note-Taker
  ## Resources and Contributors
- xxx
+ Watching class videos from the express.js lessons and reviewing the code from the activities was the best resource to complete the project.  Additionally, the express.js website was very helpful too.  (http://www.expressjs.)
  ## Link
  xx
  ## Email
- xxx
+ smcgov11.11@gmail.com
     
   
--- server.js
+++ server.js
@@ -1,29 +1,34 @@
-const express = require('express');
-const path = require('path');
-const { v4: uuidv4 } = require('uuid');
-const fs = require('fs');
+const express = require('express');//Importing express
+const path = require('path');//Importing path module so that app can work with paths to files
+const { v4: uuidv4 } = require('uuid');//Importing module to create unique ids for each user entry
+const fs = require('fs');//Importing module to read and write files
 
 const app = express();
-app.use(express.static('public'));
-app.use(express.json());
+app.use(express.static('public'));//so that app can use files in the public folder
+app.use(express.json());//so that app can use json files with express 
 
+//this is the route to the notes page and code for backend responding to frontend request and sending to notes page
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/notes.html'));
 });
 
+//this is the route to the notes page and code for backend responding to frontend and sending the db.json file
 app.get('/api/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '/db/db.json'));
 });
 
+//this is the route to the notes page taking parameters and returning them as a string and adding them to the the array of objects in db.json
 app.post('/api/notes', (req, res) => {
     const newNote = req.body;
-    newNote.id = uuidv4();
-    const notes = JSON.parse(fs.readFileSync('./db/db.json', 'utf8'));
-    notes.push(newNote);
-    fs.writeFileSync('./db/db.json', JSON.stringify(notes));
-    res.json(newNote);
+    newNote.id = uuidv4();//this creates a unique id for each note using uuidv4 package
+    const notes = JSON.parse(fs.readFileSync('./db/db.json', 'utf8'));//this puts the note in db.json parsed as an object to fit the data structure
+    notes.push(newNote);//because the notes variable is an array, we can use the push method to add the new note to the array
+    fs.writeFileSync('./db/db.json', JSON.stringify(notes));//this writes the new note to the db.json file and converts the array of objects to a string
+    res.json(newNote);//this sends the new note back to the frontend
 });
 
+
+// this is the route and code to delete a note and delete that object from the array of objects in db.json
 app.delete('/api/notes/:id', (req, res) => {
     const notes = JSON.parse(fs.readFileSync('./db/db.json', 'utf8'));
     const newNotes = notes.filter(note => note.id !== req.params.id);
@@ -31,19 +36,12 @@ app.delete('/api/notes/:id', (req, res) => {
     res.json(newNotes);
 });
 
+//this is the route to the home page and to display all the notes currently in the db.json
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'));
 });
 
-
-
-
-
-
-
-
-
-
-app.listen(3001,() => {
+//this is the route and code to console log to confirm that the server is up and running
+app.listen(3001, () => {
     console.log('Server is running on port 3001');
 });
